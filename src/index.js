d336672bf68c6bd32d077f5f74a98ab0d9a9c9b1
thanhtin4401 <thanhtin4401.ufm@gmail.com>
2022-09-23 16:53:33 +0700

    delete library

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'antd/dist/antd.css'; 
-import './index.css';
-import App from './App';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "antd/dist/antd.css";
+import "./index.css";
+import App from "./App";
+
+import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { applyMiddleware, compose, createStore } from "redux";
-import {rootReducer} from "./Redux/Reducer/rootReducer"
+import { rootReducer } from "./Redux/Reducer/rootReducer";
 import thunk from "redux-thunk";
-
-
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
@@ -19,15 +16,14 @@ export const store = createStore(
   composeEnhancers(applyMiddleware(thunk))
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <Provider store = {store}>
-    <App/>
-   </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
