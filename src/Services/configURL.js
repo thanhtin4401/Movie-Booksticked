0e9221843e0e696fae9a3f484b9218caa0ce4ee0
thanhtin4401 <thanhtin4401.ufm@gmail.com>
2022-10-14 11:23:17 +0700

    add .env

--- src/Services/configURL.js
+++ src/Services/configURL.js
@@ -6,9 +6,9 @@ import {
 import { store } from "../index";
 import { localStorageService } from "./localStorageService";
 
-export const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAzMSIsIkhldEhhblN0cmluZyI6IjIzLzAyLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY3NzExMDQwMDAwMCIsIm5iZiI6MTY0ODQwMDQwMCwiZXhwIjoxNjc3MjU4MDAwfQ.0byoDjBIIS6877xg7NwEnO16v5HOltI9AatD9OLB0Ys"
+export const TOKEN = process.env.REACT_APP_API_TOKEN;
 export const https = axios.create({
-  baseURL: "https://movienew.cybersoft.edu.vn",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     TokenCybersoft: TOKEN,
     Authorization: "Bearer " + localStorageService.user.get()?.accessToken,
